9f1893b0d604881a04bb4b550d73b90d444bbd23
doankietdev <doankietdev@gmail.com>
2025-05-18 04:48:10 +0700

    refactor[discovery]

--- discovery-service/src/main/java/com/doankietdev/discoveryservice/config/SecurityConfig.java
+++ discovery-service/src/main/java/com/doankietdev/discoveryservice/config/SecurityConfig.java
@@ -1,24 +1,30 @@
 package com.doankietdev.discoveryservice.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.security.config.Customizer;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+  @Autowired
+  CorsConfigurationSource corsConfigurationSource;
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests -> authorizeRequests
-                        .anyRequest().authenticated())
-                .httpBasic(withDefaults())
-                .csrf(csrf -> csrf.disable());
-        return http.build();
-    }
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .csrf(AbstractHttpConfigurer::disable)
+        .httpBasic(Customizer.withDefaults())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource))
+        .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+    return httpSecurity.build();
+  }
 }
