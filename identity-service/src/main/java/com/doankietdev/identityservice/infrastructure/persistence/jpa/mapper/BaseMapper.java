5772cb34cbdcc1eeaad4cc918e930736c58cfe36
doankietdev <doankietdev@gmail.com>
2025-05-18 04:28:03 +0700

    fix[persistence_mapper]: fix ignore mapping base entity

--- identity-service/src/main/java/com/doankietdev/identityservice/infrastructure/persistence/jpa/mapper/BaseMapper.java
+++ identity-service/src/main/java/com/doankietdev/identityservice/infrastructure/persistence/jpa/mapper/BaseMapper.java
@@ -1,9 +1,15 @@
 package com.doankietdev.identityservice.infrastructure.persistence.jpa.mapper;
 
+import org.mapstruct.Mapping;
+
 public interface BaseMapper<D, E, Create> {
   D toDomain(E entity);
 
   E toEntity(D domain);
 
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "createdAt", ignore = true)
+  @Mapping(target = "updatedAt", ignore = true)
+  @Mapping(target = "deletedAt", ignore = true)
   E createToEntity(Create data);
 }
